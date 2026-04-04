e3a0e03c07b58fefd9ead1c6d6cad52f93275cc2
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-04 19:59:09 +0300

    fix: app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,64 @@
-import { Icon, ThemeProvider, Toaster, ToasterComponent, ToasterProvider } from '@gravity-ui/uikit';
+import { useState } from 'react';
+import {
+  Icon, ThemeProvider, Toaster, ToasterComponent, ToasterProvider, Button, TextInput, Text, Pagination,
+  type PaginationProps, 
+} from '@gravity-ui/uikit';
 import type { MarkupString } from '@gravity-ui/markdown-editor';
+import { Sun, Moon } from '@gravity-ui/icons';
 
 import { Editor } from './Editor';
-import MarkdownPreview from './MarkdownPreview';
-import { useState } from 'react';
-
-import { Button, TextInput, Card, Text } from '@gravity-ui/uikit';
-import { Gear, Sun, Moon } from '@gravity-ui/icons';
+import Note from './Note';
 
 import style from './App.module.css';
 
 const App: React.FC = () => {
+  const [state, setState] = useState({ page: 1, pageSize: 100 });
   const [value, setValue] = useState('');
   const [theme, setTheme] = useState('dark');
   const toaster = new Toaster();
+
   const handleSubmit = (value: MarkupString) => {
     console.log('Отправленное значение:', value);
     setValue(value);
   };
+  const handleUpdate: PaginationProps['onUpdate'] = (page, pageSize) =>
+    setState((prevState) => ({ ...prevState, page, pageSize }));
 
   return (
     <ThemeProvider theme={theme}>
       <ToasterProvider toaster={toaster}>
         <ToasterComponent />
+
         <div className={style.container}>
-          <Button view="outlined" size="l" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-            <Icon data={theme === 'dark' ? Sun : Moon} size={18} />
-          </Button>
-          <Text variant="header-2">Notes</Text>
+          <div className={style.header}>
+            <Text variant="header-2">Notes</Text>
+            <Button
+              view="outlined"
+              size="l"
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            >
+              <Icon
+                data={theme === 'dark' ? Sun : Moon}
+                size={18}
+              />
+            </Button>
+          </div>
+
           <div className={style.form}>
             <TextInput placeholder="Title" size="l" />
             <TextInput placeholder="Tag" size="l" />
             <Editor onSubmit={handleSubmit} />
           </div>
+          <Note value={value} />
+          <Pagination
+            page={state.page}
+            pageSize={state.pageSize}
+            total={10000}
+            onUpdate={handleUpdate}
+          />
         </div>
 
-
-
-        <Card theme="normal" size="l" className={style.preview}>
-          {/* <Text variant="header-2">some text</Text> */}
-          <MarkdownPreview
-            getValue={() => value}
-            allowHTML={true}
-            breaks={true}
-            linkify={true}
-          />
-        </Card>
-
+        {/* <Note value={value} /> */}
       </ToasterProvider>
     </ThemeProvider>
   );
