86a3d7e72cea32a4464bb0f3e0df4ba02971643f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-11 16:06:05 +0300

    fix: editor

--- client/src/pages/EditorPage/EditorPage.tsx
+++ client/src/pages/EditorPage/EditorPage.tsx
@@ -1,27 +1,21 @@
-
-import { useNavigate } from 'react-router-dom';
-
 import type { MarkupString } from '@gravity-ui/markdown-editor';
 
-import Layout from '../../Layout';
+import Layout from '../../components/Layout/Layout';
 import Form from '../../components/Form/Form';
 
 import style from './EditorPage.module.css';
 
 const EditorPage = ({ setTheme, theme, setTitle, title, handleSubmit }
-  : {
-    setTheme: React.Dispatch<React.SetStateAction<string>>;
-    setTitle: React.Dispatch<React.SetStateAction<string>>;
-    handleSubmit: (value: MarkupString) => void;
-    title: string;
-    theme: string;
-  }) => {
-    const navigate = useNavigate();
-
+: {
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+  handleSubmit: (value: MarkupString) => void;
+  title: string;
+  theme: string;
+}) => {
   return (
     <Layout setTheme={setTheme} theme={theme}>
       <div className={style.container}>
-
         <Form
           title={title}
           setTitle={setTitle}
