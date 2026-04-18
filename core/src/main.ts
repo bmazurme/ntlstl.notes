09e2fd71d86cd1776b47181cf1c8a2239630a49f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-18 18:42:22 +0300

    fix: main

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+// import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+
+export const configureCors = (app: INestApplication) => {
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    credentials: true,
+    maxAge: 3600,
+    optionsSuccessStatus: 204,
+  });
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
+  configureCors(app);
   await app.listen(3000);
 }
 bootstrap();
