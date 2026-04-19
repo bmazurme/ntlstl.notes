631f3c1a8b25abbbd5accc637b94cd513df01b1c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-19 15:58:40 +0300

    add: auth-slice

---
+++ client-auth/src/store/slices/auth-slice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import type { RootState } from '..';
+
+export interface AuthState {
+  accessToken: string | null;
+  isAuthenticated: boolean;
+  status: 'idle' | 'checking';
+}
+
+export const initialStateAuth: AuthState = {
+  accessToken: null,
+  isAuthenticated: false,
+  status: 'idle',
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialStateAuth,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.accessToken = action.payload.accessToken;
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.accessToken = null;
+      state.isAuthenticated = false;
+    },
+    setChecking: (state) => {
+      state.status = 'checking';
+    },
+  },
+});
+
+export const { setCredentials, logout, setChecking } = authSlice.actions;
+export default authSlice.reducer;
+export const authSelector = (state: RootState) => state.auth;
