d94da40afca2f403fe635af1b89fdf893a01cd07
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-23 21:37:39 +0300

    fix: ThemeWrapper

---
+++ client-auth/src/components/ThemeWrapper.tsx
@@ -0,0 +1,19 @@
+import React, { type ReactNode } from 'react';
+import { ThemeProvider } from '@gravity-ui/uikit';
+
+import { useTheme } from '../hooks/use-theme';
+
+interface ThemeWrapperProps {
+  children: ReactNode;
+}
+
+export default function ThemeWrapper({ children }: ThemeWrapperProps) {
+  const { isDark } = useTheme();
+  const theme = React.useMemo(() => isDark ? 'dark' : 'light', [isDark]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  );
+}
