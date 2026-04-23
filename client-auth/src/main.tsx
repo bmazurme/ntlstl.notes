1d2b5a4165cff36a97f7ae4278808094ce023871
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-23 21:39:14 +0300

    fix: main

--- client-auth/src/main.tsx
+++ client-auth/src/main.tsx
@@ -3,17 +3,26 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+// import './index.css';
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
 
 import App from './App.tsx';
 import { store } from './store';
 
+import ErrorBoundaryWrapper from './components/ErrorBoundaryWrapper.tsx';
+import ThemeWrapper from './components/ThemeWrapper.tsx';
+
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeWrapper>
+        <BrowserRouter>
+          <ErrorBoundaryWrapper>
+            <App />
+          </ErrorBoundaryWrapper>
+        </BrowserRouter>
+      </ThemeWrapper>
     </Provider>,
   // </StrictMode>,
 )
