b9d79fb92d913521828aa95fc48f7f8b3fe5f120
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-04-25 12:09:03 +0300

    add: post-page

---
+++ client-auth/src/pages/PostPage.tsx
@@ -0,0 +1,15 @@
+import style from './PostPage.module.css';
+
+function PostPage() {
+  return (
+    <>
+      <section className={style.container}>
+        <div className={style.content}>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default PostPage;
