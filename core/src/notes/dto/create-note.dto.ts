1b547b1854d9c96aa0d480e7c707a63b2004ef65
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-25 21:11:21 +0300

    add: CreateNoteDto

---
+++ core/src/notes/dto/create-note.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsNotEmpty,
+  IsObject,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+import { TypeIdDto } from '../../types/dto/type-id.dto';
+
+export class CreateNoteDto {
+  @IsString({ message: 'Название должно быть строкой' })
+  @MinLength(2, { message: 'Название должно содержать минимум 2 символа' })
+  @MaxLength(100, { message: 'Название не может превышать 100 символов' })
+  title: string;
+
+  @IsString({ message: 'Название должно быть строкой' })
+  content: string;
+
+  @Type(() => TypeIdDto)
+  @IsObject({ message: 'Тип документа должен быть объектом' })
+  @IsNotEmpty({ message: 'Тип документа обязателен для указания' })
+  type: TypeIdDto;
+}
