49eab1fa951bc821dadf28f9d01822d4be5a1972
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-26 14:28:06 +0300

    fix: Type

---
+++ core/src/types/entities/type.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+import { Note } from '../../notes/entities/note.entity';
+
+@Entity()
+export class Type {
+  @PrimaryGeneratedColumn({ type: 'int', unsigned: true })
+  id: number;
+
+  @Column({
+    nullable: false,
+    length: 100,
+  })
+  name: string;
+
+  @OneToMany(() => Note, (note) => note.type)
+  notes: Note[];
+}
