4db71cfb613c622365c6f4d1cf22296b3e8c7cbc
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-27 21:12:33 +0300

    add: NotesController

--- core/src/notes/notes.controller.ts
+++ core/src/notes/notes.controller.ts
@@ -6,29 +6,35 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 
 import { NotesService } from './notes.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
+import { User } from '../users/entities/user.entity';
 
-@Controller('notes')
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { JwtGuard } from '../auth/guards/jwt.guard';
+
+@Controller('api/v1/notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
-  create(@Body() createNoteDto: CreateNoteDto) {
-    return this.notesService.create(createNoteDto);
-  }
-
-  @Get('pages/:page')
-  findAll(@Param('page') page: string) {
-    return this.notesService.findAll(+page);
+  create(@Body() createNoteDto: CreateNoteDto, @CurrentUser() user: User) {
+    return this.notesService.create(createNoteDto, user);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.notesService.findOne(+id);
+  }
+
+  @Get('pages/:page')
+  findAll(@Param('page') page: string) {
+    return this.notesService.findAll(+page);
   }
 
   @Patch(':id')
