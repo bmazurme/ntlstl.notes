18d5330a9f32aeae087e17bfefd6f3259e79992d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-27 21:12:15 +0300

    add: NotesModule

---
+++ core/src/notes/notes.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { NotesService } from './notes.service';
+import { NotesController } from './notes.controller';
+import { Note } from './entities/note.entity';
+import { TypesModule } from '../types/types.module';
+
+@Module({
+  imports: [TypesModule, TypeOrmModule.forFeature([Note])],
+  controllers: [NotesController],
+  providers: [NotesService],
+  exports: [NotesService],
+})
+export class NotesModule {}
