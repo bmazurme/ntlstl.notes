d3fd9d5c53e5ea5ea22cfe33ebfd7dcf24cfbf59
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-28 10:02:59 +0300

    fix: AppModule

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
+import { OauthModule } from './oauth/oauth.module';
 import { UsersModule } from './users/users.module';
+import { NotesModule } from './notes/notes.module';
+import { TypesModule } from './types/types.module';
+
+import { TypeOrmModuleConfig } from './type-orm.config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModuleConfig,
+    AuthModule,
+    OauthModule,
+    UsersModule,
+    NotesModule,
+    TypesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
