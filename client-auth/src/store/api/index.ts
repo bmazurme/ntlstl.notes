e89dd3fec060af0445b566c966b9b0b30ab77109
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-31 22:29:43 +0300

    fix: api

--- client-auth/src/store/api/index.ts
+++ client-auth/src/store/api/index.ts
@@ -1,3 +1,4 @@
 export { default as usersApi } from './users-api/index';
 export { default as authApi } from './auth-api/index';
 export { default as notesApi } from './notes-api/index';
+export { default as typesApi } from './types-api/index';
