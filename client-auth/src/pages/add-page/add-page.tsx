4ea95f7cf08d61f41fe7155c5eb54c881be8d148
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-03 22:21:15 +0300

    fix: add-page

--- client-auth/src/pages/add-page/add-page.tsx
+++ client-auth/src/pages/add-page/add-page.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import ContentWrapper from '../../components/ContentWrapper';
 import EditForm from '../../components/edit-form/edit-form';
 import type { FormPayload } from '../../components/edit-form/edit-form-payload';
@@ -6,11 +8,13 @@ import { useCreateNoteMutation } from '../../store';
 import style from './add-page.module.css';
 
 export default function AddPage() {
+  const navigate = useNavigate();
   const [createNote] = useCreateNoteMutation();
 
   const onSubmit = async (formData: FormPayload) => {
     try {
       await createNote(formData);
+      navigate('/');
     } catch {
       console.log('err');
     }
