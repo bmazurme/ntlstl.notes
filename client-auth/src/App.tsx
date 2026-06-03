b7f62943c2cfa0aa3dcb5b458aae3ec8b29c5511
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-03 22:22:30 +0300

    fix: app

--- client-auth/src/App.tsx
+++ client-auth/src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
-import PostPage from './pages/PostPage';
+import NotePage from './pages/note-page';
 import KitPage from './pages/KitPage';
 import ProtectedPage from './pages/ProtectedPage';
 import NotFoundPage from './pages/NotFoundPage';
+import OauthPage from './pages/OauthPage';
+import AddPage from './pages/add-page';
+import EditPage from './pages/edit-page';
 
 function App() {
   return (
@@ -12,8 +15,16 @@ function App() {
       <Routes>
         <Route path="/" element={(<MainPage page={1} />)} />
         <Route path="/notes" element={(<MainPage />)} />
-        {/* <Route path="/notes" element={<Navigate to="/" replace />} /> */}
-        <Route path="/note/:noteId" element={(<PostPage />)} />
+        <Route
+          path="/oauth"
+          element={(
+            <OauthPage />
+          )}
+        />
+        <Route path="/add-note" element={(<AddPage />)} />
+        <Route path="/edit-note/:noteId" element={(<EditPage />)} />
+        
+        <Route path="/note/:noteId" element={(<NotePage />)} />
         <Route path="/kit" element={(<KitPage />)} />
         <Route path="/profile" element={(<ProtectedPage />)} />
         <Route path="*" element={(<NotFoundPage />)} />
