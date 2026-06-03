0c0685617d4dc1117a7c4b40e2c9b023dedd06b5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-03 10:37:04 +0300

    fix: form

---
+++ client-auth/src/components/edit-form/form.fields.ts
@@ -0,0 +1,19 @@
+const fields = [
+  {
+    name: 'title',
+    label: 'Название',
+    pattern: {
+      value: /^[A-Za-zА-Яа-я0-9., -]{3,150}$/,
+      message: 'Name is invalid',
+    },
+    required: 'Обязательно к заполнению',
+    autoComplete: 'title',
+  },
+  {
+    name: 'type',
+    label: 'Описание',
+    required: 'Обязательно к заполнению',
+  },
+];
+
+export default fields;
