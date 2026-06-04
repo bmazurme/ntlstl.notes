5e9e7cee19d216900aedfac1e931d2940eaab6a7
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-04 08:04:33 +0300

    fix: sidebar

--- client-auth/src/components/Sidebar.tsx
+++ client-auth/src/components/Sidebar.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { Button, Icon } from '@gravity-ui/uikit';
+import { Plus } from '@gravity-ui/icons';
+
 export default function Sidebar() {
+  const navigate = useNavigate();
+
   return (
     <div className="sidebar">
-      <h3>sidebar</h3>
+      <Button view="action" size="m" onClick={() => navigate('/add-note')}>
+        <Icon data={Plus} size={14} />
+        Add Note
+      </Button>
+      
       <ul>
         <li>1</li>
         <li>2</li>
