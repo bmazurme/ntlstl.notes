564df114fb298b637bd3acda99f2e9f420b6502b
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-04 08:02:20 +0300

    fix: MainPage

---
+++ client-auth/src/pages/main-page/main-page.tsx
@@ -0,0 +1,13 @@
+import ContentWrapper from '../../components/ContentWrapper';
+import Posts from '../../components/notes/notes';
+
+function MainPage({ page }: { page?: number }) {
+  return (
+    <ContentWrapper
+      children={(<Posts page={page} />)}
+      sidebar
+    />
+  )
+}
+
+export default MainPage;
