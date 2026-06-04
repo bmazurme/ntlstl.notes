9ca6669007fc95f5051c0b31a710931640ff2840
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-04 08:01:57 +0300

    fix: NotePage

--- client-auth/src/pages/note-page/note-page.tsx
+++ client-auth/src/pages/note-page/note-page.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { Button, Card, Label, Modal, Skeleton, Text } from '@gravity-ui/uikit';
+import { useCallback, useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Button, Card, Icon, Label, Modal, Skeleton, Text } from '@gravity-ui/uikit';
+import { Pencil, TrashBin, ArrowLeft, CircleFill } from '@gravity-ui/icons';
 
 import ContentWrapper from '../../components/ContentWrapper';
 import { useGetNoteByIdQuery, useDeleteNoteMutation } from '../../store/api/notes-api/endpoints';
-import MarkdownPreview from '../../components/MarkdownPreview/MarkdownPreview';
+import MarkdownPreview from '../../components/markdown-preview/markdown-preview';
 import { MARKDOWN_SETTINGS } from '../../components/note/markdown-settings';
+
+import style from './note-page.module.css';
 
 export default function NotePage() {
   const { noteId } = useParams();
   const navigate = useNavigate();
+  const location = useLocation();
   const { data, isLoading } = useGetNoteByIdQuery(+noteId!);
   const [deleteNote, { isLoading: isDeleting }] = useDeleteNoteMutation();
   const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const canGoBack = location.key !== 'default';
+  const handleBack = useCallback(() => {
+    if (canGoBack) {
+      navigate(-1);
+    } else {
+      navigate('/');
+    }
+  }, [navigate, canGoBack]);
 
   const handleDelete = async () => {
     await deleteNote(+noteId!);
@@ -26,31 +39,49 @@ export default function NotePage() {
         children={(
           <Card
             className="blog-post"
-            view="filled"
+            // view="filled"
             type="container"
             size="l"
           >
+            <div className={style.header}>
+              <div>
+                <Button
+                  view="flat"
+                  size="s"
+                  onClick={handleBack}
+                >
+                  <Icon data={ArrowLeft} size={14} />
+                  All notes
+                </Button>
+                <Label icon={<Icon size={14} data={CircleFill} />}>{data?.type?.name}</Label>
+              </div>
+              <div className={style.tools}>
+                <Button
+                  view="outlined-action"
+                  size="s"
+                  onClick={() => navigate(`/edit-note/${noteId}`)}
+                >
+                  <Icon data={Pencil} size={14} />
+                  Edit
+                </Button>
+                <Button
+                  view="outlined-danger"
+                  size="s"
+                  onClick={() => setConfirmOpen(true)}
+                >
+                  <Icon data={TrashBin} size={14} />
+                  Delete
+                </Button>
+              </div>
+            </div>
             <div className="post-title">
               {isLoading
                 ? <Skeleton className="loader" />
-                : <Text variant="header-2">
+                : <Text variant="header-1">
                     {data?.title}
                   </Text>}
             </div>
-            <Button
-              view="outlined-action"
-              size="s"
-              onClick={() => navigate(`/edit-note/${noteId}`)}
-            >
-              Edit Page
-            </Button>
-            <Button
-              view="outlined-danger"
-              size="s"
-              onClick={() => setConfirmOpen(true)}
-            >
-              Delete Page
-            </Button>
+
             <div className="post-excerpt">
               {isLoading
                 ? <Skeleton className="loader-content" />
@@ -59,15 +90,6 @@ export default function NotePage() {
                     {...MARKDOWN_SETTINGS}
                   />}
             </div>
-            {isLoading
-              ? <Skeleton className="loader-content" />
-              : <Label
-                  className="post-meta"
-                  theme="normal"
-                  size="m"
-                >
-                  {data?.type?.name}
-                </Label>}
             </Card>)}
         sidebar
       />
