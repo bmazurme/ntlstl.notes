55748dd7ab27858ea8c0bb72d226374ce60168e5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-05 09:02:14 +0300

    fix: sidebar

---
+++ client-auth/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from 'react';
+import { useNavigate, useParams, useMatch } from 'react-router-dom';
+import { Button, Icon } from '@gravity-ui/uikit';
+import { Plus, Hashtag, Layers } from '@gravity-ui/icons';
+
+import ProtectedWrapper from '../protected-wrapper';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { setTypes, typesSelector, useGetTypesMutation } from '../../store';
+
+export default function Sidebar() {
+  const navigate = useNavigate();
+  const { typeId } = useParams();
+  const isAllNotes = !useMatch('/notes/type/:typeId');
+  const dispatch = useAppDispatch();
+  const types = useAppSelector(typesSelector);
+  const [getTypes] = useGetTypesMutation();
+
+  useEffect(() => {
+    const fetchTypes = async () => {
+      try {
+        const types = await getTypes().unwrap();
+        dispatch(setTypes(types));
+      } catch (err) {
+        console.log('err', err);
+      }
+    };
+
+    fetchTypes();
+  }, []);
+
+  return (
+    <div className="sidebar">
+      <ProtectedWrapper>
+        <Button view="action" size="m" onClick={() => navigate('/add-note')}>
+          <Icon data={Plus} size={14} />
+          Add Note
+        </Button>
+      </ProtectedWrapper>
+      <Button view={isAllNotes ? 'normal' : 'flat'} size="m" onClick={() => navigate('/')}>
+        <Icon data={Layers} size={14} />
+        All notes
+      </Button>
+      {types.map((type) => (
+        <Button
+          key={type.id}
+          view={typeId === String(type.id) ? 'normal' : 'flat'}
+          size="m"
+          onClick={() => navigate(`/notes/type/${type.id}`)}
+        >
+          <Icon data={Hashtag} size={14} />
+          {type.name}
+        </Button>
+      ))}
+    </div>
+  );
+}
