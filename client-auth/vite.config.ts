8e36259209fedce775981005a10289de5e7bfdbd
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-05 18:32:25 +0300

    fix: vite.config

--- client-auth/vite.config.ts
+++ client-auth/vite.config.ts
@@ -1,13 +1,18 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // test: {
-  //   globals: true,
-  //   environment: 'jsdom',
-  //   setupFiles: './src/test/setup.ts',
-  // },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+    server: {
+      deps: {
+        inline: [/@gravity-ui/],
+      },
+    },
+  },
 })
