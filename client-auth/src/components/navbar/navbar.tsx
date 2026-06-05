f01dd1c9d8f2f24c0899de95000437eb95ae1506
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-05 09:01:48 +0300

    fix: navbar

---
+++ client-auth/src/components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+// import { useNavigate } from 'react-router-dom';
+// import { Button } from '@gravity-ui/uikit';
+
+export default function Navbar() {
+  // const navigate = useNavigate();
+
+  return (
+    <nav className="blog-nav">
+      Developer notes
+    </nav>
+  );
+}
